112d7aae7d24bb53d402ac58dc9cdcf127e0a0a5
squarefong <fanghaocheng2008@126.com>
2019-04-03 11:50:00 +0800

    split head file

--- testFTP_server_manually/testftp_server.h
+++ testFTP_server_manually/testftp_server.h
@@ -46,21 +46,3 @@ public:
 //        socket->close();
 //    }
 };
-testFTPServer::testFTPServer(QObject *parent):QObject (parent){
-
-}
-testFTPServer::testFTPServer(string hostAddress, uint16_t port):socket(nullptr){
-
-    socket=new QTcpSocket(this);
-    socket->connectToHost(QString(hostAddress.c_str()), port);
-    //server->listen(QHostAddress(QString(hostAddress.c_str())), quint16(port));
-    //连接信号槽
-    connect(socket, &QTcpSocket::readyRead, this, &testFTPServer::socket_Read_Data);
-    connect(socket, &QTcpSocket::disconnected, this, &testFTPServer::socket_Disconnected);
-    //connect(IT,&inputThread::ready,this,&testFTPServer::writeDataToSocket);
-
-    connect(this, &testFTPServer::write_toSocket, this, &testFTPServer::write_DataToSocket);
-
-    qDebug() << "connected to " << QString(hostAddress.c_str()) << ":" << port;
-}
-
